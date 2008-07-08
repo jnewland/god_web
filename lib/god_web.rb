c7338484d530db817ce72fb8a1a44b9a68ea667e
Jesse Newland <jnewland@gmail.com>
2008-07-08 11:08:11 -0400

    provide a convenience method for starting GodWeb in God, adjust documentation accordingly

--- lib/god_web.rb
+++ lib/god_web.rb
@@ -5,6 +5,49 @@ class GodWeb
     ping
   end
 
+  def self.watch(options = {})
+    options[:port] ||= 8888
+    options[:environment] ||= 'production'
+    start_string = "god_web #{options[:config]} -e #{options[:environment]} -p #{options[:port]}"
+    God.watch do |w|
+      w.name              = "god_web"
+      w.interval          = 1.minute
+      w.start             = "god_web -e production -p 8888"
+      w.start_grace       = 10.seconds
+      w.restart_grace     = 10.seconds
+
+      w.start_if do |start|
+        start.condition(:process_running) do |c|
+          c.running = false
+        end
+      end
+
+      w.restart_if do |restart|
+        restart.condition(:memory_usage) do |c|
+          c.above = 20.megabytes
+          c.times = [3, 5]
+        end
+
+        restart.condition(:cpu_usage) do |c|
+          c.above = 25.percent
+          c.times = 5
+        end
+      end
+
+      w.lifecycle do |on|
+        on.condition(:flapping) do |c|
+          c.to_state = [:start, :restart]
+          c.times = 5
+          c.within = 5.minute
+          c.transition = :unmonitored
+          c.retry_in = 10.minutes
+          c.retry_times = 5
+          c.retry_within = 2.hours
+        end
+      end
+    end
+  end
+
   def setup
     DRb.start_service
     @server = DRbObject.new(nil, God::Socket.socket(@config['god_port']))
